3e000d36254364088e441aa7006388e17df06618
MuronOu <54505274+MuronOu@users.noreply.github.com>
2019-12-15 19:16:17 +0200

    Update app.js

--- app.js
+++ app.js
@@ -182,7 +182,7 @@ function getToken() {
 
 
 function sendNotification(notification) {
-    var key = 'AAAAaGQ_q2M:APA91bGCEOduj8HM6gP24w2LEnesqM2zkL_qx2PJUSBjjeGSdJhCrDoJf_WbT7wpQZrynHlESAoZ1VHX9Nro6W_tqpJ3Aw-A292SVe_4Ho7tJQCQxSezDCoJsnqXjoaouMYIwr34vZTs';
+    var key = 'AAAAXBacnMg:APA91bHoOXjrGZdeSE-ftcHODJZA7I8aognEFT8BUTY2G9ORkp5RRIniQ27NrqIbLldfo-4RQiYWdARz1gWt2oOKvLx7IfXPtbx2QrWCZf4MriQ4udfIoAjo-nqwbu6cuLgnuxg1rKZH';
 
     console.log('Send notification', notification);
 
